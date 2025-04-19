e35610cc75bad01d09a22f9ec67a143747d8a8ee
ruzen42 <malledeferlive@gmail.com>
2025-04-19 23:21:46 +0500

    add all files

---
+++ src/Configurator/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace StartConfigurator.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
